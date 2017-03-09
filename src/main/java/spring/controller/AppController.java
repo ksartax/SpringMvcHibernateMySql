17d6c7ffe7730dc373e90f00b08b20d7b4e1eb2d
Damian Stępniak <damianos-11@o2.pl>
2017-03-09 22:11:40 +0100

    i

--- src/main/java/spring/controller/AppController.java
+++ src/main/java/spring/controller/AppController.java
@@ -2,20 +2,10 @@ package spring.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import spring.dao.UserDao;
-import spring.dao.UserDaoImpl;
 import spring.service.UserService;
-
-
-import java.util.List;
 
 /**
  * Created by Damian Stępniak on 09.03.2017.
